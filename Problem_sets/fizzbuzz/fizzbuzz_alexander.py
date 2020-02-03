5e7e6b77086a6d1d998198a0ccd45bd07ef3bb55
Alexander Hinton <ahinton1991@gmail.com>
2020-02-03 08:23:16 -0800

    adding fizzbuzz soln

--- Problem_sets/fizzbuzz/fizzbuzz_alexander.py
+++ Problem_sets/fizzbuzz/fizzbuzz_alexander.py
@@ -1,9 +1,9 @@
 from test_script.test import test_fizzbuzz
 
 def fizzy(in_list):
- """
- Cheeky one-liner solution 
- """
+        """
+        Cheeky one-liner solution 
+        """
         return ["FizzBuzz" if i%3==0 and i%5==0 else "Fizz" if i%3==0 else "Buzz" if i%5==0 else i for i in in_list]
     
 if __name__ == '__main__':
